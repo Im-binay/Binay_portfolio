c812a01f1398ea047a30c1e03c4d3902ce7ba07a
BinaySharma <sharmabinay88@gmail.com>
2025-11-17 14:17:03 +0545

    Resolve page loadiong issue

--- src/components/EduExp.jsx
+++ src/components/EduExp.jsx
@@ -54,7 +54,7 @@ useLayoutEffect(() => {
   const dotColor = isDark ? '#60a5fa' : '#3b82f6';
 
   return (
-    <section id="education" className="py-8 px-3 sm:py-12  bg-light sm:px-6 md:px-12" style={{ color: textColor, backgroundColor: bgColor }}>
+    <section id="education" className="py-8 px-3 sm:py-12  sm:px-6 md:px-12" >
       
       {/* Tabs */}
       <div className="flex justify-center gap-3 sm:gap-6 mb-6 sm:mb-12">
@@ -95,7 +95,7 @@ useLayoutEffect(() => {
               {/* Accordion Header */}
               <div
                 className="ml-0 md:ml-6 w-full cursor-pointer flex flex-col sm:flex-row justify-between items-start sm:items-center p-3 sm:p-5 rounded-2xl shadow-md hover:shadow-lg transition-all duration-300"
-                style={{  color: textColor }}
+                style={{ color: textColor }}
                 onClick={() => handleToggle(idx)}
               >
                 <div className="flex-1">
